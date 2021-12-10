d67b7b9769ad5a951f6fdb6a9f87292bc0d55310
Tiana Schwarz <schwarztiana@gmail.com>
2021-12-10 01:43:33 +0000

    Code coverage pt. 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,14 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+end
