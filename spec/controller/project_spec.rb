79a91b669ac11860d75acbd3a37909ab99a88427
Tiana Schwarz <schwarztiana@gmail.com>
2021-12-10 02:04:38 +0000

    Code Coverage pt.5

--- spec/controller/project_spec.rb
+++ spec/controller/project_spec.rb
@@ -112,4 +112,29 @@ end
     end
   end
 
+  require "rails_helper"
+
+RSpec.describe Project, type: :model do
+  context "validations tests" do
+    it "ensures the title is present" do
+      project = Project.new(description: "Content of the description")
+      expect(project.valid?).to eq(false)
+    end
+
+    it "ensures the description is present" do
+      project = Project.new(title: "Title")
+      expect(project.valid?).to eq(false)
+    end
+    
+    it "should be able to save project" do
+      project = Project.new(title: "Title", description: "Some description content goes here")
+      expect(project.save).to eq(true)
+    end
+  end
+
+  context "scopes tests" do
+
+  end
+end
+
 end
